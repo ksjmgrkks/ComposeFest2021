3209cc7c213984249f4559d2a421976dc33b04d0
etomato <ksjmgrkks@naver.com>
2021-11-02 16:38:05 +0900

    2021/11/02 1. 1주차 튜토리얼 진행 (Tutorial: Jetpack Compose basics) 2. 이번주 내로 Jetpack Compose basics 코드랩 진행 후 커밋 예정

--- week 1-Jetpack Compose basics/NewProject/app/src/main/java/com/codelab/basicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/NewProject/app/src/main/java/com/codelab/basicscodelab/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.codelab.basicscodelab
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.codelab.basicscodelab.data.SampleData
 import com.codelab.basicscodelab.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +28,95 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicsCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                Conversation(SampleData.conversationSample)
+            }
+
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+
+    // padding 값 설정하기
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profileimage),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // 이미지 사이즈를 70dp로 설정
+                .size(55.dp)
+                // 이미지를 원형으로 자르기(clip)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.primary, CircleShape)
+        )
+
+        // Spacer 는 사이 공간을 설정하는 명령이다.
+        Spacer(modifier = Modifier.width(10.dp))
+
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Surface(color = MaterialTheme.colors.primary) {
+                Text(
+                    text = msg.author,
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.subtitle2
+                )
+            }
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 3,
+                    style = MaterialTheme.typography.body2
+                )
             }
         }
     }
 }
 
+//@Preview(name = "Light Mode")
+//@Preview(
+//    uiMode = Configuration.UI_MODE_NIGHT_YES,
+//    showBackground = true,
+//    name = "Dark Mode"
+//)
+//@Composable
+//fun PreviewMessageCard() {
+//    BasicsCodelabTheme {
+//        MessageCard(
+//            msg = Message("Colleague", "Take a look at Jetpack Compose, it's great!")
+//        )
+//    }
+//}
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
+fun PreviewConversation() {
     BasicsCodelabTheme {
-        Greeting("Android")
+        Conversation(SampleData.conversationSample)
     }
 }
---
+++ week 1-Jetpack Compose basics/NewProject/app/src/main/java/com/codelab/basicscodelab/data/SampleData.kt
@@ -0,0 +1,75 @@
+package com.codelab.basicscodelab.data
+
+import com.codelab.basicscodelab.Message
+
+object SampleData {
+    // Sample conversation data
+    val name = "김규성"
+    val conversationSample = listOf(
+        Message(
+            name,
+            "Test...Test...Test..."
+        ),
+        Message(
+            name,
+            "List of Android versions:\n" +
+                    "Android KitKat (API 19)\n" +
+                    "Android Lollipop (API 21)\n" +
+                    "Android Marshmallow (API 23)\n" +
+                    "Android Nougat (API 24)\n" +
+                    "Android Oreo (API 26)\n" +
+                    "Android Pie (API 28)\n" +
+                    "Android 10 (API 29)\n" +
+                    "Android 11 (API 30)\n" +
+                    "Android 12 (API 31)\n"
+        ),
+        Message(
+            name,
+            "I think Kotlin is my favorite programming language.\n" +
+                    "It's so much fun!"
+        ),
+        Message(
+            name,
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            name,
+            "Hey, take a look at Jetpack Compose, it's great!\n" +
+                    "It's the Android's modern toolkit for building native UI." +
+                    "It simplifies and accelerates UI development on Android." +
+                    "Less code, powerful tools, and intuitive Kotlin APIs :)"
+        ),
+        Message(
+            name,
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            name,
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            name,
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            name,
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            name,
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            name,
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            name,
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            name,
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+}
